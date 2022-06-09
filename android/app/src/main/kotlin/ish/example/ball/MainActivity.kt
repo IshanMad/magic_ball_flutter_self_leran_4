cce8517c5be937e2ab380e8e63b4fb230aa045e2
Ishan Madhawa <ishan@ninodigitalhouse.com>
2022-06-09 12:39:03 +0530

    finished magic ball project

---
+++ android/app/src/main/kotlin/ish/example/ball/MainActivity.kt
@@ -0,0 +1,6 @@
+package ish.example.ball
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,44 @@
 import 'package:flutter/material.dart';
-
-void main() => runApp(
-      MaterialApp(
-        home: null,
+import 'dart:math';
+void main(){
+  return runApp(
+    MaterialApp(debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: BallPage(),
       ),
+    ),
+  );
+}
+
+class BallPage extends StatefulWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  State<BallPage> createState() => _BallPageState();
+}
+
+class _BallPageState extends State <BallPage>{
+  int ballNumber = 1;
+  void changeBallFace(){
+    setState(() {
+      ballNumber = Random().nextInt(4)+1;
+
+    });
+  }
+  Widget build(BuildContext context){
+    return Center(
+     child: FlatButton(
+       onPressed: () {
+         changeBallFace();
+       },
+       child: Image.asset('images/ball$ballNumber.png'),
+     ),
     );
+  }
+  
+}
